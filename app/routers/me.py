83ee9b587e3599a012be3255ca33c5be6a6b4982
Leonardo Bezerra <pxbezerraleonardo@gmail.com>
2025-09-04 20:15:59 -0300

    Update me.py

--- app/routers/me.py
+++ app/routers/me.py
@@ -6,8 +6,8 @@ router = APIRouter()
 @router.get("/me")
 def me():
     return {"nome":"Leonardo Peixoto Xavier Bezerra",
-            "email":"leonardo43@gmail.com"
-            "curso":"sistemas de informações"
-            "github":"https://github.com/thesouthamerica/"
-            "cidade":"Juazeiro do Norte"
+            "email":"leonardo43@gmail.com",
+            "curso":"sistemas de informações",
+            "github":"https://github.com/thesouthamerica/",
+            "cidade":"Juazeiro do Norte",
             "interesses":"UFC, Youtube, Aviões"}
